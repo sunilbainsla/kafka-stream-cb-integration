f41e1c9121c86513dab49ba9ebcf4c5b47cf6b90
Sunil Kumar 8 <sunil.kumar2@publicissapient.com>
2023-06-05 14:00:19 +0100

    restter  kafka

--- src/main/java/com/sunilbainsla/kafkastreampoc/service/events/consumers/KafkaOffsetResetter.java
+++ src/main/java/com/sunilbainsla/kafkastreampoc/service/events/consumers/KafkaOffsetResetter.java
@@ -2,18 +2,20 @@ package com.sunilbainsla.kafkastreampoc.service.events.consumers;
 
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.*;
+
+import java.time.Duration;
 import java.util.*;
 
 public class KafkaOffsetResetter {
 
     public static void main(String[] args) {
-        String topic = "topic2";
+        String topic = "topic3";
         int partition = 0;
-        long desiredOffset = 22; // Specify the desired offset value
+        long desiredOffset = 2; // Specify the desired offset value
 
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "kafka-stream-poc-topic2Consumer");
+        props.put("group.id", "kafka-stream-poc-topic3Consumer");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
@@ -24,7 +26,19 @@ public class KafkaOffsetResetter {
         consumer.assign(Arrays.asList(topicPartition));
         consumer.seek(topicPartition, desiredOffset);
 
-        consumer.close();
+
+        try {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                // Process the replayed message
+                System.out.println("Replayed message: " + record.value());
+            }
+        } catch (KafkaException e) {
+            // Handle any Kafka exception
+            e.printStackTrace();
+        } finally {
+            consumer.close();
+        }
     }
 
 
