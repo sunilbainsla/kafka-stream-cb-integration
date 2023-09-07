60a4bd09f9e5d1f692d8f1d0a847739a6d87e953
Sunil Kumar <sunkumar2@WKMZT27901C8.global.publicisgroupe.net>
2023-09-07 22:19:57 +0100

    feat: sql

---
+++ src/main/resources/tws/test.sql
@@ -0,0 +1,184 @@
+
+SELECT 'N' AS change_type, u1.*
+ FROM users u1
+ LEFT JOIN users2 u2 ON u1.id = u2.id
+ WHERE u2.id IS NULL
+ 
+ UNION
+ 
+ SELECT 'D' AS change_type, u2.*
+ FROM users2 u2
+ LEFT JOIN users u1 ON u2.id = u1.id
+ WHERE u1.id IS NULL
+ 
+ UNION
+ 
+ SELECT 'U' AS change_type, u1.*
+ FROM users u1
+ JOIN users2 u2 ON u1.id = u2.id
+ WHERE u1.username <> u2.username OR u1.email <> u2.email;
+
+
+
+
+INSERT INTO users (id, username, email, change_type)
+SELECT u2.id, u2.username, u2.email, 'D' AS change_type
+FROM users2 u2
+LEFT JOIN users u1 ON u2.id = u1.id
+WHERE u1.id IS NULL;
+
+UPDATE users u
+LEFT JOIN users2 u2 ON u.id = u2.id
+SET u.change_type = 'N'
+WHERE u2.id IS NULL;
+
+UPDATE users u
+JOIN users2 u2 ON u.id = u2.id
+SET u.change_type = 'U'
+WHERE u.username <> u2.username OR u.email <> u2.email;
+
+
+
+-- Create a temporary table to store the result of the query
+CREATE TEMPORARY TABLE temp_result AS
+SELECT 'N' AS change_type, u1.*
+ FROM users u1
+ LEFT JOIN users2 u2 ON u1.id = u2.id
+ WHERE u2.id IS NULL
+ 
+ UNION
+ 
+ SELECT 'D' AS change_type, u2.*
+ FROM users2 u2
+ LEFT JOIN users u1 ON u2.id = u1.id
+ WHERE u1.id IS NULL
+ 
+ UNION
+ 
+ SELECT 'U' AS change_type, u1.*
+ FROM users u1
+ JOIN users2 u2 ON u1.id = u2.id
+ WHERE u1.username <> u2.username OR u1.email <> u2.email;
+ 
+ drop table temp_result;
+ ALTER TABLE users
+    -> DROP COLUMN change_type;
+    
+    
+    import javax.persistence.*;
+
+@Entity
+@Table(name = "your_table_name") // Replace with your actual table name
+public class YourEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    
+    private String email;
+    
+    @Transient // This annotation marks the field as non-persistent (not stored in the database)
+    private String changeType;
+
+    // Getter and setter methods for all fields, including 'changeType'
+
+    public String getChangeType() {
+        return changeType;
+    }
+
+    public void setChangeType(String changeType) {
+        this.changeType = changeType;
+    }
+}
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class YourService {
+    private final YourEntityRepository entityRepository;
+
+    @Autowired
+    public YourService(YourEntityRepository entityRepository) {
+        this.entityRepository = entityRepository;
+    }
+
+    public void updateUsers2TableBasedOnCache() {
+        // Retrieve the cached data from the entity
+        List<YourEntity> cachedData = entityRepository.getQueryResult();
+
+        // Process the cached data and perform actions based on 'changeType'
+        for (YourEntity entity : cachedData) {
+            String changeType = entity.getChangeType();
+            
+            if ("N".equals(changeType)) {
+                // Insert a new record into the 'users2' table based on 'entity' data
+                // ...
+            } else if ("D".equals(changeType)) {
+                // Delete the corresponding record from the 'users2' table based on 'entity' data
+                // ...
+            } else if ("U".equals(changeType)) {
+                // Update the corresponding record in the 'users2' table based on 'entity' data
+                // ...
+            }
+        }
+    }
+}
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class YourService {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public YourService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Cacheable(cacheNames = "#identifier", key = "#identifier") // Cache the result based on the identifier
+    public List<YourEntity> getQueryResult(String identifier) {
+        // Execute your SQL query using the jdbcTemplate
+        String sql = "SELECT 'N' AS change_type, u1.* " +
+                     "FROM users u1 " +
+                     "LEFT JOIN users2 u2 ON u1.id = u2.id " +
+                     "WHERE u2.id IS NULL " +
+                     "UNION " +
+                     "SELECT 'D' AS change_type, u2.* " +
+                     "FROM users2 u2 " +
+                     "LEFT JOIN users u1 ON u2.id = u1.id " +
+                     "WHERE u1.id IS NULL " +
+                     "UNION " +
+                     "SELECT 'U' AS change_type, u1.* " +
+                     "FROM users u1 " +
+                     "JOIN users2 u2 ON u1.id = u2.id " +
+                     "WHERE u1.username <> u2.username OR u1.email <> u2.email";
+
+        return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+            // Map the result set to your entity class
+            YourEntity entity = new YourEntity();
+            // Set entity properties from resultSet columns
+            entity.setId(resultSet.getLong("id"));
+            entity.setUsername(resultSet.getString("username"));
+            entity.setEmail(resultSet.getString("email"));
+            // Set other properties as needed
+            return entity;
+        });
+    }
+}
+
+
+@CacheEvict(cacheNames = "#identifier", key = "#identifier")
+    public void clearCache(String identifier) {
+        // This method will clear the cache for the specified identifier
+    }
