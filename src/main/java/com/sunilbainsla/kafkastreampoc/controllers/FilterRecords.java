c2e66d414a3000b29f9800def124214740b58e41
Sunil Kumar <sunkumar2@WKMZT27901C8.global.publicisgroupe.net>
2023-12-02 11:48:47 +0000

    test read logic

--- src/main/java/com/sunilbainsla/kafkastreampoc/controllers/FilterRecords.java
+++ src/main/java/com/sunilbainsla/kafkastreampoc/controllers/FilterRecords.java
@@ -8,18 +8,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FilterRecords {
-
     public static void main(String[] args) {
         String filePath = "/Users/sunkumar2/Desktop/d/FileRead2.txt";
 
         try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
             // Filter records containing the word "sunil", not containing the value "3000000",
-            // and where the substring from index 7 to 10 is not equal to "LL"
-            List<String> filteredLines = stream.filter(line -> {
-                return line.contains("sunil") &&
-                        !line.contains("3000000") &&
-                        !getSubstringAtIndex(line, 7, 10).equals("LL");
-            }).collect(Collectors.toList());
+            // and where the substring from index 7 to 10 is equal to "LL", "YY", or "ZZ"
+            List<String> filteredLines = stream
+                    .filter(line -> line.contains("sunil") && !line.contains("3000000") &&
+                            isSubstringEqual(line, 7, 10, "LL", "YY", "ZZ"))
+                    .collect(Collectors.toList());
 
             // Print the filtered lines
             filteredLines.forEach(System.out::println);
@@ -29,11 +27,15 @@ public class FilterRecords {
         }
     }
 
-    private static String getSubstringAtIndex(String line, int startIndex, int endIndex) {
-        if (startIndex >= 0 && startIndex < line.length() && endIndex >= 0 && endIndex <= line.length()) {
-            return line.substring(startIndex, endIndex);
-        } else {
-            return "";
+    private static boolean isSubstringEqual(String line, int startIndex, int endIndex, String... values) {
+        String substring = (startIndex >= 0 && startIndex < line.length() && endIndex >= 0 && endIndex <= line.length()) ?
+                line.substring(startIndex, endIndex) : "";
+
+        for (String value : values) {
+            if (substring.equals(value)) {
+                return true;
+            }
         }
+        return false;
     }
 }
